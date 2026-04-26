3578eb0e1dbd37b68a29dc6c25f93a86578d61da
VolodymyrBlyntsov <volodymyr.blyntsov@gmail.com>
2026-04-26 15:19:38 +0300

    feat: added linter

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+// @ts-check
+
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+import playwright from 'eslint-plugin-playwright'
+
+
+export default defineConfig(
+  js.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+            allowDefaultProject: ["eslint.config.mjs"]
+        },
+        tsconfigRootDir: import.meta.dirname
+      },
+    },
+  },
+  {
+    rules: {
+        '@typescript-eslint/no-explicit-any': 'warn',
+    }
+  },
+  {
+    files: ['tests/**'],
+    extends: [playwright.configs['flat/recommended']],
+    rules: {
+      // Customize Playwright rules
+      // ...
+    },
+  },
+);
